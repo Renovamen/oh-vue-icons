6caf2e2091524a39f76ac5ae38ff7f53a165f7fa
Renovamen <renovamenzxh@gmail.com>
2020-12-06 19:45:33 +0800

    chore: resize some of the icons for consistency

--- build/glyphs2json.js
+++ build/glyphs2json.js
@@ -1,11 +1,14 @@
-// Forked from https://github.com/riobard/svgfont2js, I just upgrade `xml2js` 
-// and `svgpath` to the latest version, or some generated SVGs may be broken.
+/* 
+Adopted from https://github.com/riobard/svgfont2js, I just:
+    - Upgrade `xml2js` and `svgpath` to the latest version, or some generated SVGs may be broken.
+    - Resize some of the icons.
+*/ 
 
 Object.defineProperty(exports, "__esModule", {
     value: true
 })
 
-exports.default = function (xml) {
+exports.default = function (xml, iconset) {
     const glyphs = []
 
     new _xml2js.Parser({ async: false }).parseString(xml, (err, root) => {
@@ -20,11 +23,19 @@ exports.default = function (xml) {
             const hoy = +font.$['horiz-origin-y'] || 0  // y origin of font coordinates
             const hdx = +font.$['horiz-adv-x'] || em  // width of glyph
             const vdy = +font.$['vert-adv-y'] || em  // height of glyph
-
+            
             for (let g of font.glyph) {
                 if (!g.$.d || !g.$.unicode) continue  // skip empty glyph without path data
+                
+                const ghdx = +g.$['horiz-adv-x'] || hdx
+                const gvdy = +g.$['vert-adv-y'] || vdy
+                
+                let width = ghdx > gvdy ? ghdx : gvdy
+                let height = ghdx > gvdy ? ghdx : gvdy
+                
+                if (iconset !== 'ri') width *= 1.17, height *= 1.17
 
-                const path = new _svgpath2.default(g.$.d).translate(-hox, -hoy)  // move to origin (0, 0) in font coordinates
+                const path = new _svgpath.default(g.$.d).translate(-hox + (width - ghdx) / 2, -hoy - (height - gvdy) / 2)  // move to origin (0, 0) in font coordinates
                                                         .translate(0, -ascent)  // move below x-axis
                                                         .scale(1, -1)  // invert y-axis (font coordinates -> initial coordinates)
                                                         .round(1).toString()
@@ -36,8 +47,8 @@ exports.default = function (xml) {
                     unicode: g.$.unicode,
                     unicode_hex: g.$.unicode.charCodeAt(0).toString(16),
                     path: path,
-                    width: +g.$['horiz-adv-x'] || hdx,
-                    height: +g.$['vert-adv-y'] || vdy
+                    width: width,
+                    height: height
                 })
             }
         }
@@ -47,8 +58,7 @@ exports.default = function (xml) {
 }
 
 var _xml2js = require('xml2js')
-var _svgpath = require('svgpath')
-var _svgpath2 = _interopRequireDefault(_svgpath)
+var _svgpath = _interopRequireDefault(require('svgpath'))
 
 function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : { default: obj }
