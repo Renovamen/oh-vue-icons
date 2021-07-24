9235fb97fb198502024bc431ca679866e9f0d6b9
Xiaohan Zou <renovamenzxh@gmail.com>
2021-07-24 22:22:12 +0800

    feat: fix update script, upgrade iconpacks
    - Bootstrap Icons (v1.5.0)
    - Simple Icons (v5.7.0)
    - Octicons (v14.2.2)
    - Simple Icons (v5.7.0)

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,12 @@ do
   if [ -d $dir ] && [ ! $dir = "./iconpacks/gameicons" ]
   then
     echo Updating $dir ...
-    (cd $dir; git pull origin master)
+
+    if [ $dir = "./iconpacks/bootstrap-icons" ] || [ $dir = "./iconpacks/octicons" ]
+    then
+      (cd $dir && git pull origin main)
+    else
+      (cd $dir && git pull origin master)
+    fi
   fi
 done
